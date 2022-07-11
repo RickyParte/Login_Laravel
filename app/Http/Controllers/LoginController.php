ae8060a435e94f83bbf1d25b85186c944f716e8c
RickyParte <rickydipakparte@gmail.com>
2022-07-11 16:13:46 +0530

    API

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -25,8 +25,8 @@ class LoginController extends Controller
         // Check which user has same password as inputed password
         foreach($check as $c)
         {
-        $password=$c->password;
-        $sessionUser=$c->name;
+            $password=$c->password;
+            $sessionUser=$c->name;
             // if password match then redirected to dashboard else give error
             if(Hash::check($request->password, $password))
             {
@@ -45,6 +45,12 @@ class LoginController extends Controller
     // Second Logic
     function loginUser(Request $request)
     {
+        $request->validate([
+            "email"=>"required|email",
+            "password"=>"required"
+        ]);
+
+
         // get user where email same as inputed email
         $getCredential=User::where('email',$request->email)->first();
 
@@ -59,6 +65,7 @@ class LoginController extends Controller
         {
             return redirect('/')->with("error","Please Enter Correct Credentials!");
         }
+
 
     }
 
@@ -92,4 +99,114 @@ class LoginController extends Controller
         }
         return redirect('/')->with("message","Logout Successfully!!");
     }
+
+    function registerAccount(Request $request)
+    {
+
+        $request->validate([
+            "name"=>"required|max:50|min:3",
+            "email"=>"required|email|unique:App\Models\User,email",
+            "password"=>"required|min:5"
+        ]);
+
+
+
+            $name=$request->name;
+            $email=$request->email;
+            $password=Hash::make($request->password);
+
+            $user=DB::insert('insert into users (name,email,password) values (?, ?, ?)', [$name, $email, $password]);
+            if($user)
+            {
+                return redirect('/register')->with('message',"Register Successfully...");
+            }
+            else
+            {
+                return redirect('/register')->with('error',"Not Registered...");
+            }
+    }
+
+    function try(Request $req)
+    {
+        $rule=[
+            "email"=>"required|email",
+            "number"=>"required"
+        ];
+
+        $val=Validator::make($req->all(),$rule);
+        if($val->fails())
+        {
+            // return $val->errors();
+            $errors=response()->json(['errors'=>$val->errors()]);
+            return redirect('/try')->with('errors',$errors);
+        }
+        else{
+            return $req->all();
+        }
+    }
+
+    function loginUserApi(Request $request)
+    {
+        $rules=array(
+            "email"=>"required|email",
+            "password"=>"required"
+        );
+
+        $validation=Validator::make($request->all(),$rules);
+        if($validation->fails())
+        {
+            return $validation->errors();
+        }
+
+
+
+        // get user where email same as inputed email
+        $getCredential=User::where('email',$request->email)->first();
+
+        // user is not null and password same as database password then login otherwise error
+        if($getCredential && Hash::check($request->password, $getCredential->password))
+        {
+            $sessionUser=$getCredential->name;
+            // $request->session()->put('user', $sessionUser);
+            return "Login Successfully. UserID is: ".$sessionUser;
+        }
+        else
+        {
+            return "Incorrect Credentials! Please Try again";
+        }
+
+
+    }
+
+    function registerApi(Request $request)
+    {
+
+        $rules=[
+            "name"=>"required|max:50|min:3",
+            "email"=>"required|email|unique:App\Models\User,email",
+            "password"=>"required|min:5"
+        ];
+
+        $validation=Validator::make($request->all(),$rules);
+
+        if($validation->fails())
+        {
+            return $validation->errors();
+        }
+
+        $name=$request->name;
+        $email=$request->email;
+        $password=Hash::make($request->password);
+        $user=DB::insert('insert into users (name,email,password) values (?, ?, ?)', [$name, $email, $password]);
+        if($user)
+        {
+            return "Register Successfully...";
+        }
+        else
+        {
+            return "Not Registered...";
+        }
+    }
+
+
 }
